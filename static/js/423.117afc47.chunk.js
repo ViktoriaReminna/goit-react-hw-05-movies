2a0c68da4d6e8e0f04c27178a591fc873a06a182
ViktoriaReminna <121427776+ViktoriaReminna@users.noreply.github.com>
2023-10-18 17:01:50 +0000

    Deploying to gh-pages from @ ViktoriaReminna/goit-react-hw-05-movies@c1bd92ba42d040a1a74f6d8b870d7fbc0b82a0f5 🚀

--- static/js/423.117afc47.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[423],{3423:function(t,e,a){a.d(e,{And:function(){return l}});var r=a(6069);function l(t){return(0,r.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{fillRule:"evenodd",d:"M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"}}]})(t)}}}]);
-//# sourceMappingURL=423.117afc47.chunk.js.map
